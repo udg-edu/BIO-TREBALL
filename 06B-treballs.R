e9bcd9f71c09dbc4d285f4b4eca0c74b31b6eaf3
mcomas <marc.comas@gmail.com>
2023-05-30 09:51:14 +0200

    Es llegeixen els treballs rebuts.

--- 06B-treballs.R
+++ 06B-treballs.R
@@ -1,89 +1,44 @@
 library(tidyverse)
-
-unzip("3103G000842021 Estadística aplicada-TREBALL 2n lliurament-1168815.zip", exdir = 'treballs')
+source('00-config.R')
+unzip("3103G000842022 Estadística aplicada-TREBALL 2n lliurament-1329677.zip", exdir = 'treballsB')
 treballs = tibble(
-  'fitxer' = list.files('treballs', recursive = TRUE, full.names = TRUE)) %>%
+  'fitxer' = list.files('treballsB', recursive = TRUE, full.names = TRUE)) %>%
   mutate(
-    nom_usuari = str_match(fitxer, "treballs/(.*)_\\d{7}_assignsubmission_file_/(.*)$")[,2],
-    nom_arxiu = str_match(fitxer, "treballs/(.*)_\\d{7}_assignsubmission_file_/(.*)$")[,3]
-  ) %>% left_join(read_csv('courseid_29686_participants.csv') %>%
+    nom_usuari = str_match(fitxer, "treballsB/(.*)_\\d{7}_assignsubmission_file_/(.*)$")[,2],
+    nom_arxiu = str_match(fitxer, "treballsB/(.*)_\\d{7}_assignsubmission_file_/(.*)$")[,3]
+  ) %>% left_join(read_csv('courseid_32554_participants.csv') %>%
                     transmute(
                       udg_id = paste0('u', `Número ID`),
-                      nom_complet = paste(Cognoms, Nom)
+                      nom_complet = paste(Cognoms, Nom, sep=', '),
+                      nom_complet = gsub("Torrentà i Pato", "Pato i Torrentà", nom_complet),
                     ), by = c('nom_usuari' = 'nom_complet'))
 
-load('02B-grups.RData')
+load('10-avaluacio.RData')
+groups_clean = dnotes_individuals %>% 
+  select(id, id_group) %>%
+  bind_rows(tibble('id' = "u1980918", 'id_group' = "2002963"))
+
+treballs %>%
+  anti_join(groups_clean , by = c('udg_id' = 'id') )
+
 treballs_entregats = groups_clean %>%
   inner_join( treballs, by = c('id' = 'udg_id') ) %>%
   group_by(id_group) %>%
   slice(1) %>%
   ungroup()
 
+
 correctors = groups_clean %>%
   semi_join(treballs_entregats, by = 'id_group')
 
 
 with(treballs_entregats, walk2(id_group, fitxer, function(id, fname){
-  file.copy(fname, sprintf("06B-treballs/treball_2_%d.pdf", id))
+  file.copy(fname, sprintf("06B-treballs/treball_2_%s.pdf", id))
 }))
 
 save(treballs_entregats, correctors, file = '06B-treballs.RData')
 
 writeLines(sprintf("%s", paste(unique(correctors$id), collapse = ' ')), con = 'udg_codis_correctors_B.txt')
-writeLines(sprintf("%s", paste(c('2002963',unique(treballs_entregats$id_group)), collapse = ' ')), con = 'codis_treballs_B.txt')
+writeLines(sprintf("%s", paste(unique(treballs_entregats$id_group), collapse = ' ')), con = 'codis_treballs_B.txt')
 
-rmarkdown::render("06B-treballs.Rmd", output_file = "docs/2021/treballsB.html")
-
-# 
-# ########
-# #######33
-# i_treballs = treballs_entregats$id_group
-# correctors = groups_clean %>%
-#   semi_join(treballs_entregats, by = 'id_group') %>%
-#   mutate(
-#     treballs_corr = map(id_group, ~setdiff(i_treballs, .x))
-#   )
-# 
-# N_CORR = n_distinct(correctors$id)
-# N_TREB = n_distinct(treballs_entregats$id_group)
-# N_ratio = floor(N_CORR / N_TREB)
-# 
-# N_CORREGITS = 2
-# N_MIN_CORREGITS = 2
-# 
-# set.seed(1)
-# REPEAT = TRUE
-# while(REPEAT){
-#   REPEAT  = FALSE
-#   dcor = unnest(correctors, treballs_corr) %>% group_by(treballs_corr) %>% sample_n(N_MIN_CORREGITS) %>% ungroup() %>%
-#     select(id, treball_corr = treballs_corr)
-#   
-#   if( (dcor %>% count(id) %>% {max(.$n)}) > N_CORREGITS) REPEAT  = TRUE
-# }
-# 
-# treballs_assignats = correctors %>%
-#   left_join(dcor) %>%
-#   group_by(id_group, id) %>%
-#   mutate(n_corr = N_CORREGITS  - length(na.omit(treball_corr))) %>%
-#   summarise(
-#     treballs = na.omit(c(treball_corr, sample(first(treballs_corr), first(n_corr))))
-#   ) %>% arrange(id_group)
-# 
-# treballs_assignats$treballs %>% table()
-# treballs_assignats$id %>% table()
-# 
-# set.seed(1)
-# treballs_assignats = treballs_assignats %>%
-#   mutate(codi = map_chr(id, ~paste(sample(LETTERS, 5, replace = TRUE), collapse = '')))
-# 
-# save(treballs_entregats, correctors, treballs, treballs_assignats, file = 'treballs.RData')
-# writeLines(sprintf("%s", paste(unique(treballs_assignats$id), collapse = ' ')),
-#            con = 'udg_codis_correctors.txt')
-# 
-# with(treballs_assignats, walk2(treballs, codi, function(x,y){
-#   fname = treballs_entregats %>% filter(id_group == x) %>% pull(fitxer)
-#   file.copy(fname, sprintf("docs/treball_%d.html", x))
-#   file.copy(fname, sprintf("docs/%s.html", y))
-#   file.copy(sprintf("docs/solucio_%s.pdf", x), sprintf("docs/solucio_%s.pdf", y))
-#   cat(sprintf("%s %s\n", x,y))
-# }))
+rmarkdown::render("06B-treballs.Rmd", output_file = "docs/2022/treballsB.html")
