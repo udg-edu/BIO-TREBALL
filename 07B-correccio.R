4411d5a76a925b5e0e2d84399df1ef5ac9e65e1e
mcomas <marc.comas@gmail.com>
2023-05-30 09:59:20 +0200

    Assignment B

--- 07B-correccio.R
+++ 07B-correccio.R
@@ -1,30 +1,44 @@
 library(tidyverse)
 load('06B-treballs.RData')
 
-set.seed(2)
-assignments = rerun(100, correctors %>%
+set.seed(10)
+treballs = correctors %>%
+  distinct(id_group) %>%
+  sample_n(n())
+v = pull(treballs)
+for(i in 1:nrow(treballs))  {
+  v = lag(v, default = last(v))
+  treballs[[sprintf("treb%d", i)]] = v
+}
+
+cyclic_assignment = lapply(1:4, function(i){
+  treballs %>%
+    select(1, (1+3*(i-1))+(1:3)) %>%
+    set_names(c('id_group', 'treb1', 'treb2', 'treb3')) %>%
+    mutate(name = paste0('id', i))
+}) %>% bind_rows()
+
+correctors = correctors %>%
   group_by(id_group) %>%
-  sample_frac(1) %>%
-  slice(1) %>%
-  ungroup() %>%
-  sample_frac(1) %>%
-  mutate(
-    treb1 = id_group[((0+1:n()) %% n()) + 1],
-    treb2 = id_group[((1+1:n()) %% n()) + 1],
-    treb3 = id_group[((2+1:n()) %% n()) + 1])) %>%
-  bind_rows(.id = 'it')  %>%
-  group_by(id) %>%
-  slice(1) %>%
+  mutate(name = paste0('id',1:n())) %>%
   ungroup()
+
+assignments = correctors %>%
+  left_join(cyclic_assignment, by = c('id_group', 'name')) %>%
+  arrange(id_group, name)
 
 
 assignments %>%
-  pivot_longer(matches('treb.')) %>%
+  select(matches('treb.')) %>%
+  pivot_longer(everything()) %>%
   count(value) %>%
-  arrange(n)
+  arrange(n) %>%
+  count(n)
+
+### Manual modifications to 'assignments' if necessary
+if(exists('07B-correccio-manual.R')) source('07B-correccio-manual.R')
 
 save(assignments, file = '07B-correccio.RData')
 
-I = 2
-assignment = assignments %>%
-  slice(I)
+
+
